9c6b954dd5d0c6691886626a68553ad4eac85e12
Vil2e <noirscene@gmail.com>
2024-05-04 15:23:26 +0800

    create role state script

---
+++ Assets/scripts/RoleState.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoleState
+{
+    float health;
+
+    public float Health
+    {
+        get { return health; }
+
+        set
+        {
+            if (value < 0) health = 0;
+            health = value;
+        }
+
+    }
+
+    public RoleState(string json)
+    {
+
+    }
+
+
+}
