2a45eb324976eec3b8a6cb57d2415324c6320582
Vil2e <noirscene@gmail.com>
2024-05-12 19:27:19 +0800

    leanTween import

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -11,6 +11,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] GameObject door;
 
+    [SerializeField] RectTransform fader;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -21,6 +23,14 @@ public class GameManager : MonoBehaviour
 
     }
 
+    private void Start()
+    {
+        fader.gameObject.SetActive(true);
+
+        LeanTween.alpha(fader, 1, 0);
+        LeanTween.alpha(fader, 0, 0.5f).setOnComplete(() => { fader.gameObject.SetActive(false); });
+    }
+
     public void LoadNextScene() 
     {
         int totalSceneAmount = SceneManager.sceneCountInBuildSettings;
@@ -29,13 +39,21 @@ public class GameManager : MonoBehaviour
 
         if(nextScene > totalSceneAmount - 1) { return; }
 
-        SceneManager.LoadScene(nextScene);
-        
+        fader.gameObject.SetActive(true);
+
+        LeanTween.alpha(fader, 0, 0);
+        LeanTween.alpha(fader, 1, 0.5f).setOnComplete(() => { SceneManager.LoadScene(nextScene); });
+
     }
 
     public void BackToMenu()
     {
-        SceneManager.LoadScene(0);
+        fader.gameObject.SetActive(true);
+
+        LeanTween.alpha(fader, 0, 0);
+        LeanTween.alpha(fader, 1, 0.5f).setOnComplete(() => { SceneManager.LoadScene(0); });
+
+        
     }
 
     public void QuitGame()
