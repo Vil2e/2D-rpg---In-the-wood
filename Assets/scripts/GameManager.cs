a66781f53728deadcb6376086618ca33f1e5fd11
Vil2e <noirscene@gmail.com>
2024-05-12 21:43:36 +0800

    change theme color

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] bool isGameStart;
+    [SerializeField] bool isGameStart;//避免在menu畫面生成player
     [SerializeField] GameObject player;
     [SerializeField] GameObject door;
 
@@ -23,13 +23,7 @@ public class GameManager : MonoBehaviour
 
     }
 
-    private void Start()
-    {
-        fader.gameObject.SetActive(true);
-
-        LeanTween.alpha(fader, 1, 0);
-        LeanTween.alpha(fader, 0, 0.5f).setOnComplete(() => { fader.gameObject.SetActive(false); });
-    }
+   
 
     public void LoadNextScene() 
     {
@@ -40,19 +34,13 @@ public class GameManager : MonoBehaviour
         if(nextScene > totalSceneAmount - 1) { return; }
 
         fader.gameObject.SetActive(true);
-
-        LeanTween.alpha(fader, 0, 0);
-        LeanTween.alpha(fader, 1, 0.5f).setOnComplete(() => { SceneManager.LoadScene(nextScene); });
-
+        SceneManager.LoadScene(nextScene);
     }
 
     public void BackToMenu()
     {
         fader.gameObject.SetActive(true);
-
-        LeanTween.alpha(fader, 0, 0);
-        LeanTween.alpha(fader, 1, 0.5f).setOnComplete(() => { SceneManager.LoadScene(0); });
-
+        SceneManager.LoadScene(0);
         
     }
 
